e186d0d40db28e8a5c5b70dfc27792821ce8f594
shamanth44 <93857544+shamanth44@users.noreply.github.com>
2024-02-12 21:51:20 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
             </NavLink>
           </div>
           <div className="col-2">
-            <img className="bannerImg" src="/Assets/banner.png" alt="" />
+            <img className="bannerImg" src="/Assets/banner.png" alt="banner" />
           </div>
         </div>
 
